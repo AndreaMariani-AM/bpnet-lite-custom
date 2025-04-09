df6062be620438b24a9c3ae1a67ba71e27bef843
Andrea Mariani <71487811+AndreaMariani-AM@users.noreply.github.com>
2025-04-09 14:24:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     url='https://github.com/AndreaMariani-AM/bpnet-lite-custom',
     license='LICENSE.txt',
     description='bpnet-lite-custom is a modification of the original bpnet-lite.',
+    python_requires='>=3.9',
     install_requires=[
         "bpnet-lite >= 0.8.1"
     ],
